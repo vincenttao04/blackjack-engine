dbd0df0abe126296b59e9b59d9fd3c3e35455c31
vincenttao04 <vincent.jh.tao@gmail.com>
2026-03-12 23:42:14 +1300

    chore: tidy console display

--- include/Rules.h
+++ include/Rules.h
@@ -2,9 +2,9 @@
 #define RULES_H
 
 struct Rules {
-    int numberOfDecks = 4;
-    bool dealerHitsSoft17 = false;
-    double blackjackPayout = 1.5;
+    int numberOfDecks;
+    bool dealerHitsSoft17;
+    double blackjackPayout;
 };
 
 #endif
