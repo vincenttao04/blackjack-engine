9bc3bf07d37763c3f0e6858c4ef7609198864c9b
vincenttao04 <vincent.jh.tao@gmail.com>
2026-03-15 14:40:00 +1300

    feat: simulate hit ev using recursion

--- src/MonteCarlo.cpp
+++ src/MonteCarlo.cpp
@@ -5,59 +5,69 @@
 
 std::pair<double, double> MonteCarlo::simulate(const GameState& state) {
     GameState simState = state;
+
+    // Hide dealer's hole card and return it to the deck
     Card card = simState.dealer.cards.back();
     simState.dealer.cards.pop_back();
     simState.shoe.cards.push_back(card);
 
-    double standEV = simulateStandEV(simState, 100);
-    double hitEV = simulateHitEV(simState, 100);
-    return {standEV, hitEV};
-}
-
-double MonteCarlo::simulateStandEV(const GameState& simState, int simulations) {
-    double total = 0.0;
+    const int simulations = 100;
+    double standEV = 0.0;
+    double hitEV = 0.0;
 
     for (int i = 0; i < simulations; i++) {
-        GameState runState = simState;
-        runState.shoe.shuffle();
+        simState.shoe.shuffle();
 
-        while (runState.dealer.value() < 17) {
-            runState.dealer.addCard(runState.shoe.draw());
-        }
+        GameState standState = simState;
+        GameState hitState = simState;
 
-        int playerValue = runState.player.value();
-        int dealerValue = runState.dealer.value();
+        standEV += simulateStand(standState);
+        hitEV += simulateHit(hitState);
 
-        if (dealerValue > 21 || playerValue > dealerValue) {
-            total += 1.0;
-        } else if (playerValue < dealerValue) {
-            total -= 1.0;
-        } else {
-            total += 0.0;
-        };
-
-        std::cout << total / (i + 1) << std::endl;
+        std::cout << "stand ev: " << standEV / (i + 1) << std::endl;
+        std::cout << "hit ev: " << hitEV / (i + 1) << std::endl;
     };
 
-    return total / simulations;
+    return {standEV / simulations, hitEV / simulations};
 }
 
-double MonteCarlo::simulateHitEV(const GameState& simState, int simulations) {
-    double total = 0.0;
-    for (int i = 0; i < simulations; i++) {
-        GameState runState = simState;
-        runState.shoe.shuffle();
+double MonteCarlo::simulateStand(GameState& state) {
+    while (state.dealer.value() < 17) {
+        state.dealer.addCard(state.shoe.draw());
+    }
 
-        runState.player.addCard(runState.shoe.draw());
+    int playerValue = state.player.value();
+    int dealerValue = state.dealer.value();
 
-        if (runState.player.isBust()) {
-            total -= 1.0;
-        } else {
-            total += 1.0;
-        }
+    if (dealerValue > 21 || playerValue > dealerValue) {
+        return 1.0;
+    } else if (playerValue < dealerValue) {
+        return -1.0;
+    }
 
-        std::cout << total / (i + 1) << std::endl;
+    return 0.0;
+}
+
+double MonteCarlo::simulateHit(GameState& state) {
+    state.player.addCard(state.shoe.draw());
+
+    if (state.player.isBust()) {
+        return -1.0;
     };
 
-    return total / simulations;
+    return simulateDecision(state);
+}
+
+double MonteCarlo::simulateDecision(GameState& hitState) {
+    if (hitState.player.value() >= 21) {
+        return simulateStand(hitState);
+    }
+
+    GameState standState = hitState;
+    GameState nextHitState = hitState;
+
+    double standEV = simulateStand(standState);
+    double hitEV = simulateHit(nextHitState);
+
+    return std::max(standEV, hitEV);
 }
