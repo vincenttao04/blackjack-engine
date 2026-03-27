617688a1fe8ad0446ed9cc8a63592e9d5b97468f
Vincent Tao <vincent.jh.tao@gmail.com>
2026-03-27 19:17:12 +1300

    feat: autoplay backtest

--- src/Backtest.cpp
+++ src/Backtest.cpp
@@ -3,11 +3,29 @@
 #include <iostream>
 
 #include "Game.h"
+#include "GameState.h"
+#include "MonteCarlo.h"
 
 using namespace std;
 
+void Backtest::autoPlayerTurn(GameState& state) {
+    while (true) {
+        if (state.player.isBust()) {
+            break;
+        }
+
+        auto [standEV, hitEV] = MonteCarlo::simulate(state);
+
+        if (standEV >= hitEV) {
+            state.player.addCard(state.shoe.draw());
+        } else {
+            return;
+        }
+    }
+};
+
 void Backtest::run() {
-    int numberOfTests = 100;
+    const int numberOfTests = 100;
     int winCount = 0;
     int pushCount = 0;
     int loseCount = 0;
@@ -16,12 +34,33 @@ void Backtest::run() {
 
     state.shoe.initialize();
 
-    if (state.shoe.needsReshuffle()) {
-        state.shoe.initialize();
-        cout << "Reshuffling..." << endl;
-    };
+    for (int i = 0; i < numberOfTests; i++) {
+        if (state.shoe.needsReshuffle()) {
+            state.shoe.initialize();
+            cout << "Reshuffling..." << endl;
+        };
 
-    Game::playRound(state);
+        state.player.clear();
+        state.dealer.clear();
 
+        Game::dealInitialCards(state);
+        autoPlayerTurn(state);
+
+        Game::playDealer(state);
+
+        Outcome result = Game::determineOutcome(state);
+
+        if (result == Outcome::PlayerWin) {
+            winCount++;
+        };
+
+        if (result == Outcome::DealerWin) {
+            loseCount++;
+        };
+
+        if (result == Outcome::Push) {
+            pushCount++;
+        };
+    }
     return;
 };
