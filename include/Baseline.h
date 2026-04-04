f46b1d1614a99e3ee5a205d32b05fc9966ad653a
vincenttao04 <vincent.jh.tao@gmail.com>
2026-04-04 23:04:55 +1300

    feat: print baseline test results

--- include/Baseline.h
+++ include/Baseline.h
@@ -10,7 +10,7 @@ class Baseline {
    private:
     static void autoPlayerTurn(GameState& state);
     static void autoDealerTurn(GameState& state);
-    // static void printResults(int wins, int loses, int pushes);
+    static void printResults(int wins, int loses, int pushes);
 };
 
 #endif
